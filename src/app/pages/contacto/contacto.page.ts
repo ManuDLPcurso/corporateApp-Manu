00ba5750eaccc43935c6084c1d423dec2a95dae7
manana <manana@Mac-mini-de-administrator.local>
2026-07-01 14:00:50 +0200

    Haciendor Formulario

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonTextarea, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ProductsService } from 'src/app/services/products.service';
+import { Preferences } from '@capacitor/preferences';
+import { MessagesService } from 'src/app/services/messages.service';
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButton,IonInput,IonTextarea,IonItem]
 })
 export class ContactoPage implements OnInit {
 
-constructor() {}
+correo:string="";
+mensaje:string="";
+
+constructor(private messagesService:MessagesService) {
+  async guardar() {
+await this.messagesService.guardarDato(this.correo, this.mensaje);
+}
+async leer() {
+correo = await this.messagesService.obtenerDato(this.correo);
+}
+}
+
+async enviar(){
+await Preferences.set({
+  key: 'ultimoMensaje',
+  value: JSON.stringify({
+  correo:this.correo,
+  mensaje:this.mensaje
+})
+})
+}
+
+
+
 
  ngOnInit() {}
 
 }
+
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  
+
+    async guardarDato(correo: string, mensaje: string) {
+  await Preferences.set({ mensaje });
+}
+async obtenerDato(correo: string, mensaje: string) {
+  const result = await Preferences.get({ mensaje });
+  return result.value;
+}
 }
